b3fd73fdeb3a667a2dd8035a5af0ec084ae4f4a2
root <root@localhost.localdomain>
2019-08-06 12:17:24 +0800

    addlogin

--- www/js/index.js
+++ www/js/index.js
@@ -23,13 +23,15 @@ function getData(){
     });
 }
 
-function initMenu(data){
+function init(data){
     var li="";
     for(var d in data){
         li+='<li><a href="'+(data[d].url||"javascript:;")+'" target="_blank">'+data[d].title+'</a></li>';
     }
     $("#products").empty();
     $("#products").html(li);
+
+    setLogin($.cookie('LiSA.login'))
 }
 
 var loadBg= function(reFresh){
@@ -47,14 +49,44 @@ var loadBg= function(reFresh){
     $(".bg").css("background-image",'url("img//'+conf.bgImg[n]+'")');
 }
 
+
+var setLogin = function(setValue){
+    var value = setValue || $(".login").text()
+    value = value.replace(/[^\a-\z\A-\Z0-9]/g,'')
+    if(value.length>6){
+        value= value.substr(0,8)
+    }
+    if(!value){
+        $(".login").text("login")
+        $.cookie('LiSA.login',"", {expires: 7,path:'/'});
+    }else{
+        $(".login").text(value)
+        $.cookie('LiSA.login',value, {expires: 7,path:'/'});
+    }
+
+}
 $(function () {
     getConfig();
     getData();
-    initMenu(data);
-    loadBg()
     $(".refresh").click(function () {
         $(this).attr("disabled",true);
         loadBg(true)
         $(this).attr("disabled",false);
     });
+    $(".login").focus(function(){
+        var name = $.trim($(".login").text())
+        if(name && name == "login"){
+            $(".login").text("")
+        }
+    })
+    $(".login").blur(function(){
+        setLogin()        
+    })
+    $(".login").keydown(function(event){
+        if (event.keyCode == 13) {
+            $(".login").blur()
+        }
+    })
+    init(data);
+    loadBg()
 });
